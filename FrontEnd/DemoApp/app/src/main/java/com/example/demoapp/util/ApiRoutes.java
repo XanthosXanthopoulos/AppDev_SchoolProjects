e85a3a109360ea0554c1de83efdbf2d232534921
Nikos Siachamis <nikosgr9917@gmail.com>
2021-05-13 17:34:34 +0300

    change settings

--- FrontEnd/DemoApp/app/src/main/java/com/example/demoapp/util/ApiRoutes.java
+++ FrontEnd/DemoApp/app/src/main/java/com/example/demoapp/util/ApiRoutes.java
@@ -5,7 +5,7 @@ import java.util.Map;
 
 public final class ApiRoutes
 {
-    private static final String BASE = "http://192.168.1.109:5000/api";
+    private static final String BASE = "http://192.168.2.8:5000/api";
 
     public static final String LOGIN = "/account/login";
     public static final String REGISTER = "/account/register";
