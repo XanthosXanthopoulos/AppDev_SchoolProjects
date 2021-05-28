625cb76302d519d1ecd6893e5ddc02dcffdeec7e
XanthosXanthopoulos <p3170125@aueb.gr>
2021-05-29 02:41:57 +0300

    Minor improvements

---
+++ BackEnd/WebServer/WebServer/Utilities/EnumUtilities.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace WebServer.Utilities
+{
+    public class EnumUtilities
+    {
+        public static T GetValueFromDescription<T>(string description) where T : Enum
+        {
+            foreach (var field in typeof(T).GetFields())
+            {
+                if (Attribute.GetCustomAttribute(field,
+                typeof(DescriptionAttribute)) is DescriptionAttribute attribute)
+                {
+                    if (attribute.Description == description)
+                        return (T)field.GetValue(null);
+                }
+                else
+                {
+                    if (field.Name == description)
+                        return (T)field.GetValue(null);
+                }
+            }
+
+            throw new ArgumentException("Not found.", nameof(description));
+        }
+    }
+}
